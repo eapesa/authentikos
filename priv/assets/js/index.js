887432aa8c6f1e9deb0838e72cce654255103ad8
Elixa-Mae Pesa <eapesa@gmail.com>
2018-03-25 15:29:12 +0800

    Added layout handler for verification of passcode

--- priv/assets/js/index.js
+++ priv/assets/js/index.js
@@ -2,18 +2,50 @@ new Vue({
   el: "#app",
   data: {
     title: "Authentikos",
-    identity: "",
-    qrCode: ""
+    qrCode: "",
+    flags: {
+      passcode: false,
+      checkResult: false
+    },
+    generateInputs: {
+      account_name: ""
+    },
+    verifyInputs: {
+      account_name: "",
+      passcode    : ""
+    }
   },
   methods: {
     generateQrCode: function(e) {
       e.preventDefault();
-      var params = { account_name: this.identity }
+      var params = this.generateInputs;
       this.$http.get("/otp/generate", { params: params })
         .then((response) => {
           qrBlob = response.body;
+          this.flags.checkResult = false;
           this.qrCode = URL.createObjectURL(qrBlob);
+        }).catch((error) => {
+          console.log(error);
         });
+    },
+
+    verifyQrCode: function(e) {
+      e.preventDefault();
+      var params = this.verifyInputs;
+      this.$http.get("/otp/verify", { params: params })
+        .then((response) => {
+          return response.json();
+        }).catch((error) => {
+          console.log(error);
+        }).then((json) => {
+          status = json.code;
+          this.flags.checkResult = true;
+          if (status != "OK") {
+            this.flags.passcode = false;
+          } else {
+            this.flags.passcode = true;
+          }
+        })
     }
   }
 })
